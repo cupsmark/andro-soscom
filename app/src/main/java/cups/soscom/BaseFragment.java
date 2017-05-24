60a0273541b4fabf770318964818a53ad0465452
bayulaksana <bayulaksana89@gmail.com>
2017-05-24 11:05:04 +0700

    Update Folder

--- app/src/main/java/cups/soscom/BaseFragment.java
+++
@@ -1,9 +0,0 @@
-package cups.soscom;
-
-import android.app.Fragment;
-
-/**
- * Created by ekobudiarto on 5/8/17.
- */
-public class BaseFragment extends Fragment {
-}
--- app/src/main/java/cups/soscom/fragment/FragmentAuthLogin.java
+++
@@ -1,7 +0,0 @@
-package cups.soscom.fragment;
-
-/**
- * Created by ekobudiarto on 5/8/17.
- */
-public class FragmentAuthLogin {
-}
